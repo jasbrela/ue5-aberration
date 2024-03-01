881bd262145cde8bcb366a2a9b773774f8aeb0eb
Gabriela Matias <jasbrela@gmail.com>
2024-03-01 23:15:29 +0000

    create project

---
+++ Source/Aberration/AberrationGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AberrationGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAberrationGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAberrationGameMode();
+};
+
+
+
