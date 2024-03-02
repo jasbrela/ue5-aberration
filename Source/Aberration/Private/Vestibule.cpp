e9fe59f00cfaf23c89abf165e8d16a6a3ce40679
Gabriela Matias <jasbrela@gmail.com>
2024-03-02 14:00:41 +0000

    seamless transition

---
+++ Source/Aberration/Private/Vestibule.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Vestibule.h"
+
+#include "Aberration/DebugMacros.h"
+#include "Components/BoxComponent.h"
+
+AVestibule::AVestibule()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	SetRootComponent(BoxCollision);
+}
+
+void AVestibule::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (bCanTeleport)
+	{
+		BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnBeginOverlap);
+		LOG("Registered OnBeginOverlap");
+	}
+
+	Location = GetActorLocation();
+}
+
+void AVestibule::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	LOG("Overlapped with %s", *OtherActor->GetName());
+	
+	FVector OtherLocation = OtherActor->GetActorLocation();
+	FVector NewLocation = OtherVestibule->GetRelativePosition(GetOffset(OtherActor));
+	
+	OtherActor->SetActorLocation(FVector(NewLocation.X, NewLocation.Y, OtherLocation.Z));
+}
+
+FVector AVestibule::GetRelativePosition(FVector Offset)
+{
+	return Location + Offset;
+}
+
+FVector AVestibule::GetOffset(AActor* Actor)
+{
+	return Actor->GetActorLocation() - Location;
+}
+
+void AVestibule::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Aberration/Public/Vestibule.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Vestibule.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class ABERRATION_API AVestibule : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AVestibule();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleDefaultsOnly)
+	UBoxComponent* BoxCollision;
+
+	UPROPERTY(EditInstanceOnly)
+	bool bCanTeleport;
+
+	UPROPERTY(EditInstanceOnly)
+	AVestibule* OtherVestibule;
+
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	FVector GetRelativePosition(FVector Offset);
+
+	FVector GetOffset(AActor* Actor);
+	
+	FVector Location;
+};
